2103902312a9611d8b33c334296177bfd1977a2d
j.connor.obrien@gmail.com <j.connor.obrien@gmail.com>
2021-05-23 23:32:41 -0400

    Almost Finished

--- Develop/script.js
+++ Develop/script.js
@@ -21,39 +21,67 @@ WHEN I refresh the page
 THEN the saved events persist
 */
 
-// 1. Display Current Day at the top of the calendar
-
+var timeBlocks = [
+    {displayTime:"9 AM", time24h:"9", description:"",},
+    {displayTime:"10 AM", time24h:"10", description:"",},
+    {displayTime:"11 AM", time24h:"11", description:"",},
+    {displayTime:"12 PM", time24h:"12", description:"",},
+    {displayTime:"1 PM", time24h:"13", description:"",},
+    {displayTime:"2 PM", time24h:"14", description:"",},
+    {displayTime:"3 PM", time24h:"15", description:"",},
+    {displayTime:"4 PM", time24h:"16", description:"",},
+    {displayTime:"5 PM", time24h:"17", description:"",},
+  ];  
 function displayCurrentDate() {
     var todayData = luxon.DateTime.now();
     var todaysDate = todayData.toLocaleString(luxon.DateTime.DATE_HUGE);
     $('#currentDay').text(todaysDate);
 };
-// 2. Time blocks for standard business hours (9-5)
-//     A. Color Coded (Past, Present, Future)
-//     B. OnClick - Enter an event and save, text is saved in local storage (Persists on refresh)
-/*
-<div class="row time-block">
-    <div class="col-1 hour">9AM</div>
-    <div class="col-10 description textarea">World</div>
-    <div class="col-1 saveBtn">420</div>
-</div> */
+
+
+function saveSchedule(){
+    localStorage.setItem("Schedule", JSON.stringify(timeBlocks));
+};
 
 function renderTimeBlocks(){
     var currentHour = luxon.DateTime.now();
     currentHour = currentHour.hour;
-    var timeBlocks = {9:'9 AM', 10:'10 AM', 11:'11 AM',12:'12 PM',13:'1 PM',14:'2 PM', 15:'3 PM', 16:'4 PM', 17:'5 PM'};
-    
-    for (const [key, value] of Object.entries(timeBlocks)) {
-        var hour = `${key}`;
+
+    if (localStorage.getItem("Schedule") === null){
+        localStorage.setItem("Schedule", JSON.stringify(timeBlocks));
+    }
+    var data = JSON.parse(localStorage.getItem("Schedule"));;
+
+    for (var i=0; i<timeBlocks.length; i++) {
+
+        var hour = data[i].time24h;        
         var blockList = document.querySelector("#block-list");
 
         var timeBlockEl = document.createElement("div");
         timeBlockEl.classList = "row time-block";
-
+        //Time 
         var timeEl = document.createElement("div");
         timeEl.classList = "col-1 hour";
-      
+        timeEl.innerHTML = data[i].displayTime;
+
+        //Save Button
+        var saveEl = document.createElement("div");
+        saveEl.classList = "col-1 btn btn-primary saveBtn";
+        saveEl.id = "save";
+        saveEl.innerHTML = "Save";
+        
         var eventEl = document.createElement("div");
+        var eventDescriptionEl = document.createElement("textarea");
+
+        eventDescriptionEl.classList = "textarea";
+        eventDescriptionEl.id = "description";
+        if(data[i].description == ""){
+            eventDescriptionEl.textContent = "Enter Text";
+        }
+        eventDescriptionEl.textContent = data[i].description;
+
+        eventEl.appendChild(eventDescriptionEl);
+
         //Past
         if (hour < currentHour){
             eventEl.classList = "col-10 past";
@@ -66,58 +94,37 @@ function renderTimeBlocks(){
         else if (hour == currentHour){
             eventEl.classList = "col-10 present";
         }
-
-        var saveEl = document.createElement("div");
-        saveEl.classList = "col-1 .saveBtn";
-
         //Assemble Time Block
         timeBlockEl.appendChild(timeEl);
         timeBlockEl.appendChild(eventEl);
         timeBlockEl.appendChild(saveEl);
-
         blockList.appendChild(timeBlockEl);
-      
+        
     }
 }
-// MVP
-// 1. Display Current Day on top 
+
+
 displayCurrentDate();
-// 2. Create 1 time Block - Function 
-
 renderTimeBlocks();
-// 3. Be able to edit the time block - function
-// 4. Click Save to save the time block
-// 
 
-/*
-for (var i = 0; i < repos.length; i++) {
-    //format repo name
-    var repoName = repos[i].owner.login + "/" + repos[i].name;
+$( ".saveBtn" ).click(function() {
 
-    // create a container for each repo
-    var repoEl = document.createElement("div");
-    repoEl.classList = "list-item flex-row justify-space-between align-center";
+    const index = $(this)
+    .closest(".time-block")
+    .index();
 
-    // create a span element to hold repo name 
-    var titleEl = document.createElement("span");
-    titleEl.textContent = repoName;
+    const eventDescription = $(this)
+    .closest(".time-block")
+    .find(".textarea")
+    .val();
 
-    // append to container
-    repoEl.appendChild(titleEl);
+    timeBlocks[index].description = eventDescription;
+    console.log(index);
+    console.log(eventDescription);
+    console.log(timeBlocks[index]);
+    localStorage.setItem("Schedule", JSON.stringify(timeBlocks));
 
-    var statusEl = document.createElement("span");
-    statusEl.classList = "flex-row align-center";
+});
 
-    // check if current repo has issues or not
-    if (repos[i].open_issues_count > 0) {
-        statusEl.innerHTML = "<i class= 'fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-    } else {
-        statusEl.innerHTML = "<i class= 'fas fa-check-square status-icon icon-success'></i>";
-    }
-    // append to container
-    repoEl.appendChild(statusEl);
-    //append container to the dom
-    repoContainerEl.appendChild(repoEl);
-}
-}
-*/
+
+
